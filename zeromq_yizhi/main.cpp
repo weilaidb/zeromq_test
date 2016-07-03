233d542cc43fa3619c468cf3b9f09f33866073d3
Your Name <you@example.com>
2016-07-03 10:37:28 +0800

    ZeroMQ 2.2.0 Qt移植，编译通过

---
+++ zeromq_yizhi/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "zmq.h"
+
+
+using namespace std;
+
+
+void hello()
+{
+//    /* TCP port 5555 on all available interfaces */
+//    rc = zmq_bind(socket, "tcp://*:5555");
+//    assert (rc == 0);
+//    /* TCP port 5555 on the local loop-back interface on all platforms */
+//    rc = zmq_bind(socket, "tcp://127.0.0.1:5555");
+//    assert (rc == 0);
+//    /* TCP port 5555 on the first Ethernet network interface on Linux */
+//    rc = zmq_bind(socket, "tcp://eth0:5555");
+//    assert (rc == 0);
+}
+
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
+

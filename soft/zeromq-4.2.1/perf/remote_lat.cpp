33ece3538efddb4df54a8b39e437a6610f268482
Your Name <you@example.com>
2017-02-13 22:17:59 +0800

    generate remote_lat.exe

--- soft/zeromq-4.2.1/perf/remote_lat.cpp
+++ soft/zeromq-4.2.1/perf/remote_lat.cpp
@@ -32,6 +32,33 @@
 #include <stdlib.h>
 #include <string.h>
 
+//#include <zmq.h>  
+#include <string.h>  
+#include <stdio.h>  
+//#include <unistd.h>  
+
+int main(void)
+{
+	printf("Connecting to hello world server...\n");
+	void *context = zmq_ctx_new();
+	void *requester = zmq_socket(context, ZMQ_REQ);
+	zmq_connect(requester, "tcp://localhost:5555");
+
+	int request_nbr;
+	for (request_nbr = 0; request_nbr != 10; request_nbr++) {
+		char buffer[10];
+		printf("Sending Hello %d...\n", request_nbr);
+		zmq_send(requester, "Hello", 5, 0);
+		zmq_recv(requester, buffer, 10, 0);
+		printf("Received World %d\n", request_nbr);
+	}
+	zmq_close(requester);
+	zmq_ctx_destroy(context);
+	return 0;
+}
+
+#if 0
+
 int main (int argc, char *argv [])
 {
     const char *connect_to;
@@ -127,3 +154,6 @@ int main (int argc, char *argv [])
 
     return 0;
 }
+
+
+#endif
